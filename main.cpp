65edfd7eda5daecb70f6c685c8066e1b3aa431ce
Chris Jacobsen <datahead8888@gmail.com>
2016-04-17 19:12:54 -0400

    Add commenting

--- main.cpp
+++ main.cpp
@@ -16,13 +16,14 @@ void doUpdate(float timeElapsed);
 void doRender(sf::RenderWindow & window);
 
 //Global variables - a quick solution for a simple prototype for now
-float x = 300, y = 300;
-sf::Vector2f velocity(0.0f, 0.0f);
-bool running = true;
-const float SPEED = 50.0f;
+float x = 300, y = 300;  //Position of circle object
+sf::Vector2f velocity(0.0f, 0.0f); //Velocity of circle object
+bool running = true;  //True if app is running
+const float SPEED = 50.0f; //Speed to assign to each component of velocity when moving
 
 int main()
 {
+	//Test joystick connectivity
 	if (!sf::Joystick::isConnected(0))
 	{
 		cerr << "First joystick is not connected" << endl;
@@ -32,6 +33,7 @@ int main()
 
 	sf::Clock clock; //Clock to represent time elapsed
 
+	//Main loop
 	while (running)
 	{
 		sf::Event windowEvent;
@@ -48,6 +50,8 @@ int main()
 	return 0;
 }
 
+//This method responds to SFML events
+//Parameter windowEvent - the SFML event to interpret
 void handleEvent(sf::Event & windowEvent)
 {
 
@@ -56,6 +60,7 @@ void handleEvent(sf::Event & windowEvent)
 		case sf::Event::Closed:
 			running = false;
 			break;
+		//Keyboard events
 		case sf::Event::KeyPressed:
 			switch (windowEvent.key.code)
 			{
@@ -63,7 +68,7 @@ void handleEvent(sf::Event & windowEvent)
 					running = false;
 					break;
 				case sf::Keyboard::Up:
-					velocity.x = 0.0f;
+					velocity.x = 0.0f; //This is not ideal for diagonal movement but is fine for a first prototype
 					velocity.y = -SPEED;
 					cout << "Up key pressed" << endl;
 					break;
@@ -85,6 +90,7 @@ void handleEvent(sf::Event & windowEvent)
 					break;
 			}
 			break;
+		//Joystick events - for now just print them onscreen for our analysis
 		case sf::Event::JoystickMoved:
 			switch (windowEvent.joystickMove.axis)
 			{
@@ -100,16 +106,19 @@ void handleEvent(sf::Event & windowEvent)
 
 }
 
-
+//Physics update method
+//timeElapsed - amount of time elapsed for this time step
 void doUpdate(float timeElapsed)
 {
+	//Integrate the change in position
 	x += velocity.x * timeElapsed;
 	y += velocity.y * timeElapsed;
 }
 
+//Rendering method
+//Parameter window - the SFML window object used
 void doRender(sf::RenderWindow & window)
 {
-
 	window.clear(sf::Color::Black);
 
 	sf::CircleShape shape(10);
